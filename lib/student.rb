ecf8a587c8d2211a9e3fd49636f5ff3d1a4fe41a
Chandelier Ephrata <chandephrata@gmail.com>
2022-05-10 18:34:03 -0600

    commit tree b997b872d0acd0bc8d4b58514a46bf23eae01fef

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+#Change the class definition so that the Teacher class inherits from the User class. so i added < User on class User
+class Student < User
+    attr_reader :knowledge
+
+#Individual students should initialize with an instance variable, @knowledge, that points to an empty array.
+    def initialize
+        @knowledge = []
+    end
+
+#Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+    def learn (knowledge)
+        @knowledge << knowledge
+    end 
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+#Define the User class such that a user can have a first and last name. 
+
+class User
+    attr_accessor :first_name, :last_name
+end
